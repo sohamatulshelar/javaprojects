82895804f142f26123a5ed63faf6264531dfefc9
sohamatulshelar <sohamshelar2430@gmail.com>
2024-02-12 23:45:48 +0530

    Combination java file

---
+++ Combination.java
@@ -0,0 +1,22 @@
+class Combination
+{
+    public static void main(String args[])
+    {
+        char a='0';
+        char b='1';
+        byte c=101;
+        short d=10;
+        int e=120;
+        float f=12.10f;
+
+        System.out.println("addition is "+a+b);
+        System.out.println(a+b+c+d);
+        System.out.println(b+c);
+        System.out.println((char)(a+b));
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println((float)(c+d+e+f));
+
+    }
+}
+
